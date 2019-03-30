2f1282fdd165c507100a46ec3342fb09870b9e93
allBames <asbalmasov@gmail.com>
2019-03-30 23:28:49 +0300

    Добавлен человек

--- src/ru/geekbrains/lesson1/animal/Human.java
+++ src/ru/geekbrains/lesson1/animal/Human.java
@@ -1,63 +1,16 @@
 package ru.geekbrains.lesson1.animal;
 
-import ru.geekbrains.lesson1.Participant;
 import ru.geekbrains.lesson1.enums.Color;
 
-public class Human extends Animal implements Participant {
+public class Human extends Animal {
 
-    private boolean isOnDistance;
-    private int runDistance;
-    private int jumpHeight;
-
-    public Human(String name, Color color, int age, int runDistance, int jumpHeight) {
-        super(name, color, age);
-        this.isOnDistance = true;
-        this.runDistance = runDistance;
-        this.jumpHeight = jumpHeight;
+    public Human(String name, Color color, int age, int maxRunDistance,
+               int maxJumpHeight, int maxSwimDistance) {
+        super(name, color, age, maxRunDistance, maxJumpHeight, maxSwimDistance);
     }
-
 
     @Override
     public void voice() {
-        System.out.println("Привет");
-    }
-
-    @Override
-    public boolean isOnDistance() {
-        return isOnDistance;
-    }
-
-    @Override
-    public void run(int distance) {
-        if (!isOnDistance) {
-            return;
-        }
-        if (distance > runDistance) {
-            isOnDistance = false;
-            return;
-        }
-        System.out.println(String.format("Человек %s пробежал кросс длинной %d", getName(), distance));
-    }
-
-    @Override
-    public void jump(int height) {
-        if (!isOnDistance) {
-            return;
-        }
-        if (height > jumpHeight) {
-            isOnDistance = false;
-            return;
-        }
-        System.out.println(String.format("Человек %s пругнул на высоту %d", getName(), height));
-    }
-
-    @Override
-    public void swim(int distance) {
-        isOnDistance = false;
-        System.out.println("Человек не умеет плавать");
-    }
-
-    public void setRunDistance(int runDistance) {
-        this.runDistance = runDistance;
+        System.out.println("Ура!");
     }
 }
