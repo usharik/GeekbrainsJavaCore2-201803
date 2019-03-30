ad9562305d644a46f59a27ad65d4a3bdb353fea3
allBames <asbalmasov@gmail.com>
2019-03-30 23:30:46 +0300

    Добавлен метод showResults()

--- src/ru/geekbrains/lesson1/Team.java
+++ src/ru/geekbrains/lesson1/Team.java
@@ -1,19 +1,32 @@
 package ru.geekbrains.lesson1;
 
+import ru.geekbrains.lesson1.animal.Animal;
+import ru.geekbrains.lesson1.animal.Cat;
+
 /**
  * Класс - комманда участников соревнований
  */
 public class Team {
 
+    String name;
+
     private Participant[] participants;
 
     // здесь используется конструктор с переменным числом параметров
-    public Team(Participant... participants) {
+    public Team(String name, Participant... participants) {
         // внутри метода переменное число параметров интерпретируется как массив
         this.participants = participants;
+        this.name = name;
     }
 
     public Participant[] getParticipants() {
         return participants;
+    }
+
+    public void showResults() {
+        System.out.println("Результаты для команды " + '"' + name + '"' + ":");
+        for (Participant participant : participants) {
+            System.out.println(participant);
+        }
     }
 }
