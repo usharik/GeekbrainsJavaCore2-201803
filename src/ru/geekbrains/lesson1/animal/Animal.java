60a4f5cf935f73a540db9e18de8812855039a3a8
allBames <asbalmasov@gmail.com>
2019-03-30 23:30:03 +0300

    Изменен класс

--- src/ru/geekbrains/lesson1/animal/Animal.java
+++ src/ru/geekbrains/lesson1/animal/Animal.java
@@ -1,35 +1,89 @@
 package ru.geekbrains.lesson1.animal;
 
+import ru.geekbrains.lesson1.Participant;
 import ru.geekbrains.lesson1.enums.Color;
 
-public abstract class Animal {
+public abstract class Animal implements Participant {
 
     private String name;
     private Color color;
     private int age;
 
-    public Animal(String name, Color color, int age) {
+    private boolean isOnDistance;
+    private int runDistance;
+    private int jumpHeight;
+    private int swimLength;
+
+    public Animal(String name, Color color, int age, int runDistance, int jumpHeight, int swimLength) {
         this.name = name;
         this.color = color;
         this.age = age;
-    }
-
-    // Перегруженный (overload) конструктор
-    public Animal(String name, Color color) {
-        this(name, color, 0);
-//        this.name = name;
-//        this.color = color;
-//        this.age = 0;
+        this.runDistance = runDistance;
+        this.jumpHeight = jumpHeight;
+        this.swimLength = swimLength;
+        this.isOnDistance = true;
     }
 
     public abstract void voice();
 
-    public String getName() {
-        return name;
+    public boolean isOnDistance() {
+        return isOnDistance;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void run(int distance) {
+        if (!isOnDistance) {
+            return;
+        }
+        if (distance > runDistance) {
+            isOnDistance = false;
+            System.out.println(name  + " не смог(ла) пробежать расстояние " + distance + "км.");
+            return;
+        }
+        else {
+            System.out.println(name  + " смог(ла) пробежать расстояние " + distance + "км.");
+            return;
+        }
+
+    }
+
+    public void jump(int height) {
+        if (!isOnDistance) {
+            return;
+        }
+        if (height > jumpHeight) {
+            isOnDistance = false;
+            System.out.println(name  + " не смог(ла) перепрыгнуть стену " + height + "м.");
+            return;
+        }
+        else {
+            System.out.println(name  + " смог(ла) перепрыгнуть стену " + height + "м.");
+        }
+    }
+
+    public void swim(int distance) {
+        if (!isOnDistance) {
+            return;
+        }
+        if (swimLength == 0){
+            isOnDistance = false;
+            System.out.println(name + " не умеет плавать!");
+        }
+        else if(distance > swimLength){
+            isOnDistance = false;
+            System.out.println(name + " не проплыл(а)" + distance + "м.");
+        }
+        else {
+            System.out.println(name + " проплыл(а)" + distance + "м.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name + (isOnDistance() ? " прош(ел/ла) соревнования!" : "не справил(ся/ась) с соревнованиями!");
+    }
+
+    public String getName() {
+        return name;
     }
 
     public Color getColor() {
@@ -40,7 +94,4 @@ public abstract class Animal {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
