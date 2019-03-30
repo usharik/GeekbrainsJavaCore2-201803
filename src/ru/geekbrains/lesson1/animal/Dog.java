6386a1e7bcdd7df8a9bf40fe2fd64782e7a35efe
allBames <asbalmasov@gmail.com>
2019-03-30 23:29:15 +0300

    Добавлена собака

--- src/ru/geekbrains/lesson1/animal/Dog.java
+++ src/ru/geekbrains/lesson1/animal/Dog.java
@@ -1,63 +1,16 @@
 package ru.geekbrains.lesson1.animal;
 
-import ru.geekbrains.lesson1.Participant;
 import ru.geekbrains.lesson1.enums.Color;
 
-public class Dog extends Animal implements Participant {
+public class Dog extends Animal {
 
-    private boolean isOnDistance;
-    private int runDistance;
-    private int jumpHeight;
-
-    public Dog(String name, Color color, int age, int runDistance, int jumpHeight) {
-        super(name, color, age);
-        this.isOnDistance = true;
-        this.runDistance = runDistance;
-        this.jumpHeight = jumpHeight;
+    public Dog(String name, Color color, int age, int maxRunDistance,
+               int maxJumpHeight, int maxSwimDistance) {
+        super(name, color, age, maxRunDistance, maxJumpHeight, maxSwimDistance);
     }
-
 
     @Override
     public void voice() {
-        System.out.println("Гав");
-    }
-
-    @Override
-    public boolean isOnDistance() {
-        return isOnDistance;
-    }
-
-    @Override
-    public void run(int distance) {
-        if (!isOnDistance) {
-            return;
-        }
-        if (distance > runDistance) {
-            isOnDistance = false;
-            return;
-        }
-        System.out.println(String.format("Собака %s пробежала кросс длинной %d", getName(), distance));
-    }
-
-    @Override
-    public void jump(int height) {
-        if (!isOnDistance) {
-            return;
-        }
-        if (height > jumpHeight) {
-            isOnDistance = false;
-            return;
-        }
-        System.out.println(String.format("Собака %s пругнула на высоту %d", getName(), height));
-    }
-
-    @Override
-    public void swim(int distance) {
-        isOnDistance = false;
-        System.out.println("Собака не умеет плавать");
-    }
-
-    public void setRunDistance(int runDistance) {
-        this.runDistance = runDistance;
+        System.out.println("Мяу");
     }
 }
