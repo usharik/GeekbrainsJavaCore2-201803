de649b132e454ba2ff22672db974be454432afee
Sergei_Smirnov <ser2603@yandex.ru>
2019-04-10 03:39:10 +0500

    Дз_3 выполнено

--- src/ru/geekbrains/lesson3/PhoneBook.java
+++ src/ru/geekbrains/lesson3/PhoneBook.java
@@ -1,4 +1,30 @@
 package ru.geekbrains.lesson3;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class PhoneBook {
+
+    Map<String, Set<String>> phoneBook;
+
+    public PhoneBook() {
+        this.phoneBook = new HashMap<>();
+    }
+
+    public Set<String> getNumber(String name) {
+        return phoneBook.get(name);
+    }
+
+    public void setNumber(String name, String number) {
+
+        Set<String> phones = phoneBook.get(name);
+        if (phones == null) {
+            phones = new HashSet<>();
+            phoneBook.put(name, phones);
+        }
+        phones.add(number);
+    }
 }
+
