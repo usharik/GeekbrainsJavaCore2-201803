598f0eb99deb8c5e08e0422c5ecd3d1c4822e853
usharik <usharik@mail.ru>
2019-04-10 11:02:33 +0200

    Lesson4 JavaFX minimal app

---
+++ src/ru/geekbrains/lesson4/javafx/Controller.java
@@ -0,0 +1,39 @@
+package ru.geekbrains.lesson4.javafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class Controller {
+
+    @FXML
+    public TextArea messages;
+
+    @FXML
+    public TextField messageText;
+
+    private Stage primaryStage;
+
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+    public void onSendClicked() {
+        // TODO реализовать добавление сообщений в TextArea messages
+        showModalAlert("Network chat",
+                "Message",
+                "Message text",
+                Alert.AlertType.INFORMATION);
+    }
+
+    private static void showModalAlert(String title, String header, String message, Alert.AlertType alertType) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(message);
+
+        alert.showAndWait();
+    }
+}
