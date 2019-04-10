4dc6d514fc8b38b41aeb4875cba630fb2c02bde0
ksks <bogacheva.minna@yandex.ru>
2019-04-10 22:51:17 +0300

    Второе домашнее задание. Добавление обработки отрицательных чисел и parseInt.

--- src/ru/geekbrains/lesson2/Main.java
+++ src/ru/geekbrains/lesson2/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
     public static void main(String[] args){
 
-        String[][] arrStr = new String[4][4];
+        String[][] arrStr = new String[4][3];
         int m = 10;
         for (int i = 0; i < arrStr.length; i++){
             for (int j = 0; j < arrStr[i].length; j ++){
-                arrStr[i][j] = "-" + m + ".5";
+                arrStr[i][j] = "-" + m + ".5r";
                 m++;
                 System.out.print(arrStr[i][j] + " ");
             }
--- src/ru/geekbrains/lesson2/MyArray.java
+++ src/ru/geekbrains/lesson2/MyArray.java
@@ -1,6 +1,8 @@
 package ru.geekbrains.lesson2;
 
 
+import java.text.NumberFormat;
+
 class MyArray{
 
     final private static int SIZE_ARR = 4;
@@ -8,7 +10,8 @@ class MyArray{
     public static double getArray(String[][] numArr) throws MyArraySizeException, MyArrayDataException{
         int i;
         double result = 0;
-        int status;
+        String vvv;
+        NumberFormat nf = NumberFormat.getInstance();
 
         if (numArr.length != SIZE_ARR) throw new MyArraySizeException("строк", SIZE_ARR);
         for (i = 0; i < numArr.length; i ++){
@@ -17,20 +20,19 @@ class MyArray{
 
         for (i = 0; i < numArr.length; i++){
             for (int j = 0; j < numArr[i].length; j ++){
-                if (numArr[i][j] == null || !numArr[i][j].matches("^[-+]?[0-9]*[.,]?[0-9]+$")) status = 0;
-                else status = 1;
-                /*try {
-                    Integer.parseInt(numArr[i][j]);
-                    status = 1;
-                } catch (Exception e) {
-                    status = 0;
-                }*/
-
-                if (status == 1){//isNumber(numArr[i][j])){
+                vvv = numArr[i][j];
+                /*if (nf.parse(vvv))  { //numArr[i][j] == null || !numArr[i][j].matches("^[-+]?[0-9]*[.,]?[0-9]+$")) {
                     result += Double.parseDouble(numArr[i][j].replace(',','.'));
                 }
-                else throw new MyArrayDataException(numArr[i][j], i, j);
+                else throw new MyArrayDataException(numArr[i][j], i, j);*/
+                try{
+                    result += Double.parseDouble(numArr[i][j].replace(',','.'));
+                }
+                catch(NumberFormatException  e){
+                    throw new MyArrayDataException(numArr[i][j], i, j);
+                }
             }
+
         }
         return result;
     }
