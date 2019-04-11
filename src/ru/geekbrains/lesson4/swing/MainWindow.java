8713e1086968bcff9f3efcefebef24d9c760b261
SoBoLp <pbolotov@gmail.com>
2019-04-11 20:48:48 +0200

    Home work 4 done

--- src/ru/geekbrains/lesson4/swing/MainWindow.java
+++ src/ru/geekbrains/lesson4/swing/MainWindow.java
@@ -9,7 +9,7 @@ public class MainWindow extends JFrame {
 
     public MainWindow() {
         setTitle("Application");
-        setBounds(200,200, 500, 500);
+        setBounds(200, 200, 500, 500);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         setLayout(new BorderLayout());
@@ -17,6 +17,7 @@ public class MainWindow extends JFrame {
         JTextArea messagesArea = new JTextArea();
         messagesArea.setLineWrap(true);
         messagesArea.setWrapStyleWord(true);
+        messagesArea.setEditable(false);
 
         JScrollPane scroll = new JScrollPane(messagesArea,
                 JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
@@ -26,18 +27,22 @@ public class MainWindow extends JFrame {
         JPanel sendMessagePanel = new JPanel();
         sendMessagePanel.setLayout(new BorderLayout());
         JButton sendButton = new JButton("Отправить");
-        sendButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(MainWindow.this,
-                        "Message", "Title", JOptionPane.INFORMATION_MESSAGE);
-            }
-        });
         sendMessagePanel.add(sendButton, BorderLayout.EAST);
         JTextField messageField = new JTextField();
         sendMessagePanel.add(messageField, BorderLayout.CENTER);
 
         add(sendMessagePanel, BorderLayout.SOUTH);
+
+        Action sendMessage = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                messagesArea.append(messageField.getText() + '\n');
+                messageField.setText("");
+            }
+        };
+
+        sendButton.addActionListener(sendMessage);
+        messageField.addActionListener(sendMessage);
 
         setVisible(true);
     }
