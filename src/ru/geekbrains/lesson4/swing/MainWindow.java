b11fcbf99e16f19b4bfe891858b123d58ebb8e4f
voodoo420 <voodoo420@mail.ru>
2019-04-12 14:55:37 +0300

    homework 4

--- src/ru/geekbrains/lesson4/swing/MainWindow.java
+++ src/ru/geekbrains/lesson4/swing/MainWindow.java
@@ -7,16 +7,19 @@ import java.awt.event.ActionListener;
 
 public class MainWindow extends JFrame {
 
+    private JTextArea messagesArea = new JTextArea();
+    private JTextField messageField = new JTextField();
+
     public MainWindow() {
-        setTitle("Application");
+        setTitle("Chat");
         setBounds(200,200, 500, 500);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         setLayout(new BorderLayout());
 
-        JTextArea messagesArea = new JTextArea();
         messagesArea.setLineWrap(true);
         messagesArea.setWrapStyleWord(true);
+        messagesArea.setEditable(false);
 
         JScrollPane scroll = new JScrollPane(messagesArea,
                 JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
@@ -25,20 +28,40 @@ public class MainWindow extends JFrame {
 
         JPanel sendMessagePanel = new JPanel();
         sendMessagePanel.setLayout(new BorderLayout());
+        sendMessagePanel.add(messageField, BorderLayout.CENTER);
+        add(sendMessagePanel, BorderLayout.SOUTH);
+
+        messageField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                printMsg(messageField.getText());
+            }
+        });
+
         JButton sendButton = new JButton("Отправить");
         sendButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(MainWindow.this,
-                        "Message", "Title", JOptionPane.INFORMATION_MESSAGE);
+                printMsg(messageField.getText());
             }
         });
-        sendMessagePanel.add(sendButton, BorderLayout.EAST);
-        JTextField messageField = new JTextField();
-        sendMessagePanel.add(messageField, BorderLayout.CENTER);
 
-        add(sendMessagePanel, BorderLayout.SOUTH);
+        sendMessagePanel.add(sendButton, BorderLayout.EAST);
 
         setVisible(true);
     }
+
+
+    private void printMsg(String msg){
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                if(msg.equals("")) return;
+                messagesArea.append(msg + "\n");
+                messagesArea.setCaretPosition(messagesArea.getDocument().getLength());
+                messageField.setText("");
+            }
+        });
+    }
+
 }
