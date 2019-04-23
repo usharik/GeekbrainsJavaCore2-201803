aed4eb0efb14d1901fd81c952bb4ab83e9959c05
ksks <bogacheva.minna@yandex.ru>
2019-04-23 07:54:04 +0300

    Недоделанное домашнее задание по 6 уроку. Сейчас есть только два потока, которые могут отправлять и принимать сообщения.

---
+++ src/ru/geekbrains/lesson6/EchoClient.java
@@ -0,0 +1,59 @@
+package ru.geekbrains.lesson6;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoClient {
+    public static void main(String[] args) throws InterruptedException {
+
+        try (Scanner scanner = new Scanner(System.in);
+             Socket socket = new Socket("localhost", 7777)) {
+
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+
+            System.out.print("Введите сообщение > ");
+
+            Thread thrSend = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (scanner.hasNextLine()) {
+                        System.out.print("Введите сообщение > ");
+                        String line = scanner.nextLine();
+                        try {
+                            out.writeUTF(line);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            break;
+                        }
+                    }
+                }
+            });
+            Thread thrGet = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            System.out.println("Новое сообщение > " + in.readUTF());
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                            break;
+                        }
+                    }
+                }
+            });
+            thrSend.start();
+            thrGet.start();
+            thrSend.join();
+            thrGet.join();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+}
---
+++ src/ru/geekbrains/lesson6/EchoServer.java
@@ -0,0 +1,95 @@
+package ru.geekbrains.lesson6;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class EchoServer {
+
+    static class GetMail implements Runnable{
+
+        DataInputStream in;
+
+        public GetMail(DataInputStream in){
+          this.in = in;
+        }
+
+        @Override
+        public void run(){
+            try {
+                System.out.println("Новое сообщение > " + in.readUTF());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+
+
+    public static void main(String[] args) throws InterruptedException {
+        try (Scanner scanner = new Scanner(System.in);
+             ServerSocket serverSocket = new ServerSocket(7777)) {
+            while (true) {
+                System.out.println("Сервер ожидает подключения!");
+                Socket socket = serverSocket.accept();
+
+                System.out.println("Кто-то подключился: " + socket.getInetAddress());
+
+                DataInputStream in = new DataInputStream(socket.getInputStream());
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+                System.out.print("Введите сообщение > ");
+
+                Thread thrGet = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (true) {
+                            try {
+                                System.out.println("Новое сообщение > " + in.readUTF());
+                            } catch (IOException ex) {
+                                ex.printStackTrace();
+                                break;
+                            }
+                        }
+                    }
+                });
+
+                Thread thrSend = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //System.out.print("Введите сообщение > ");
+                        while (scanner.hasNextLine()) {
+                            System.out.print("Введите сообщение > ");
+                            String line = scanner.nextLine();
+                            try {
+                                out.writeUTF(line);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                break;
+                            }
+                        }
+                    }
+                });
+
+
+                    thrSend.start();
+                    thrGet.start();
+                    thrGet.join();
+                    thrSend.join();
+                    /*try {
+                        System.out.println("Новое сообщение > " + in.readUTF());
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                        break;
+                    }*/
+
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
