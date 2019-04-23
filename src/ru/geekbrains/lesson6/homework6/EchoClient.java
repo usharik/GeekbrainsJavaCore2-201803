46c1a23be6b87206019d1eff286af93032f041e8
Sergei_Smirnov <ser2603@yandex.ru>
2019-04-23 16:19:56 +0500

    Hw6

--- src/ru/geekbrains/lesson6/homework6/EchoClient.java
+++ src/ru/geekbrains/lesson6/homework6/EchoClient.java
@@ -1,4 +1,21 @@
 package ru.geekbrains.lesson6.homework6;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class EchoClient {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        try (Scanner scanner = new Scanner(System.in);
+             Socket socket = new Socket("localhost", 7777)) {
+
+            Transmitter transmitter = new Transmitter(scanner, socket);
+            transmitter.msgTrans();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
--- src/ru/geekbrains/lesson6/homework6/Listener.java
+++ src/ru/geekbrains/lesson6/homework6/Listener.java
@@ -1,4 +1,26 @@
 package ru.geekbrains.lesson6.homework6;
 
-public class Listener {
+import java.io.DataInputStream;
+import java.io.IOException;
+
+public class Listener implements Runnable{
+
+    DataInputStream in;
+
+    public Listener(DataInputStream in) {
+        this.in = in;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                System.out.println("\nНовое сообщение > " + in.readUTF());
+                System.out.print("Введите сообщение > ");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                break;
+            }
+        }
+    }
 }
